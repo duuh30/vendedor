9fe9b89fd326cb8f4a6efc9655a782963ce51e8e
Eduardo Augusto <duuh-l@hotmail.com>
2018-06-15 14:57:40 -0300

    Editar e Deletar
    Editando e Deletando os dados com sucesso !

--- editardadosfrutas.php
+++ editardadosfrutas.php
@@ -8,7 +8,7 @@
 	if (!empty($nomeFruta && $quantidadeFruta && $preco && $tipoFruta)){
 				echo "Registro foi alterado com sucesso !";
 			$conn->query("UPDATE frutas
-						SET nome='$nomeFruta', quantidade=$quantidadeFruta, preco=$preco, id_tipo_fruta=$tipoFruta
+						SET nome='$nomeFruta', quantidade='$quantidadeFruta', preco='$preco', id_tipo_fruta='$tipoFruta'
 						WHERE id_fruta=$id");
 			} else {
 				echo "O registro não foi alterado, verifice se os campos estão vazios!";
